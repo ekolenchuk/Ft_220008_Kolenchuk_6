7571ea9555c36d47ede002e18bcca903a5c06f36
ekolenchuk <93521468+ekolenchuk@users.noreply.github.com>
2023-10-28 18:24:26 +0500

    Добавлено округление. Рефакторинг

--- Ft_220008_Kolenchuk_6/main.py
+++ Ft_220008_Kolenchuk_6/main.py
@@ -1,9 +1,33 @@
+from tabulate import tabulate
+
+
+def print_table(list_criteria, list_value, list_weight_factor):
+    count_criteria = len(list_criteria)
+    table = list_value.copy()
+    for i in range(count_criteria):
+        table[i].insert(0, list_criteria[i])
+        table[i].append(list_weight_factor[i])
+    list_criteria.insert(0, '')
+    list_criteria.append('Весовой коэфф')
+    table.insert(0, list_criteria)
+    print(tabulate(table))
+
+
+def round_sum(list):
+    sum = 0.0
+    for el in list:
+        round(el, 2)
+        sum += el
+    return sum
+
+
 count = int(input('Введите количество критериев:  '))
 criteria = []
 for i in range(count):
-    criteria.append(input(f'Название критерия {i+1}:  '))
+    criteria.append(input(f'Название критерия {i + 1}:  '))
 
 arr = []
+
 for i in range(count):
     arr.append([])
     for j in range(count):
@@ -11,32 +35,26 @@ for i in range(count):
 
 i = 0
 j = 0
-S = 0.0
-sum_line = [0.0]*count
+sum_line = [0.0] * count
 while i != count:
     while j != count:
         arr[i][j] = float(input(f'Относительная важность между критериями {criteria[i]} и {criteria[j]}:  '))
         sum_line[i] += arr[i][j]
         j += 1
-
     i += 1
     j = 0
 
     while j != i and i != count:
-        arr[i][j] = round(1/(arr[j][i]), 2)
+        arr[i][j] = round(1 / (arr[j][i]), 2)
         sum_line[i] += round(arr[i][j], 2)
         j += 1
 
-for el in sum_line:
-    round(el, 2)
-    S += el
-
+sum_importance = round_sum(sum_line)
 
 weight_factor = []
 for i in range(count):
-    weight_factor.append(sum_line[i]/S)
-    print(f'Весовой коэфф критерия {criteria[0]} равен {weight_factor[i]}')
+    factor = sum_line[i] / sum_importance
+    weight_factor.append(round(factor, 2))
+    # print(f'Весовой коэфф критерия {criteria[i]} равен {weight_factor[i]}')
 
-print(arr)
-print(sum_line)
-print(S)
+print_table(criteria, arr, weight_factor)
